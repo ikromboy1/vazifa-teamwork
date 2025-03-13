3ecd4f341a7f3db6bcb470e96452a6d8f55b1885
ikromboy1 <ikromboykomiljanov761@gmail.com>
2025-03-13 18:14:13 +0500

    Home sahifasiga o‘zgarishlar kiritildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import React from "react";
+import { Route, Router } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Ishni boshlayveringlar. tayyor</h1>
+    <Router>
+      <Route path="/" element={<Home/>}/>
+    </Router>
     </>
   );
 }
